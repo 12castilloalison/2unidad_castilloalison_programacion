ee56ba23fdd971c14a0bbd5d0dd0a37212b9609e
12castilloalison <castilloalison16db@gmail.com>
2026-04-13 12:21:28 -0600

    Add files via upload

---
+++ ejercicio1_switch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+internal class Program
+{
+    // un programa que solicite un numero del 1 al 7
+    //mostrar en pantalla los dias de la semana segun el numero ingresado
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("*********************");
+        Console.WriteLine("  DIAS DE LA SEMANA  ");
+        Console.WriteLine("*********************");
+        int dia;
+        Console.WriteLine("Digite un valor entero del 1 al 7");
+        dia = Convert.ToInt32(Console.ReadLine());
+
+        switch(dia)
+            {
+            case 1:
+                Console.WriteLine("LUNES");
+                break;
+            case 2:
+                Console.WriteLine("MARTES");
+                break;
+            case 3:
+                Console.WriteLine("MIERCOLES");
+                break;
+            case 4:
+                Console.WriteLine("JUEVES");
+                break;
+            case 5:
+                Console.WriteLine("VIERNES");
+                break;
+            case 6:
+                Console.WriteLine("SABADO");
+                break;
+            case 7:
+                Console.WriteLine("DOMINGO");
+                break;
+            default:
+                Console.WriteLine("ESCRIBE UN VALOR VALIDO DEL1 AL 7");
+                break;
+        }
+    }
+}
---
+++ ejercicio3.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        char letra;
+        Console.WriteLine("CLASIFICACION DE LETRAS POR RANGO");
+        Console.Write("dame una letra:  ");
+        letra = Convert.ToChar(Console.ReadLine());
+        switch(letra)
+            {
+            case 'A':
+                Console.WriteLine("EXCELENTE");
+                break;
+            case 'B':
+                Console.WriteLine("BUENO");
+                break;
+            case 'C':
+                Console.WriteLine("REULAR");
+                break;
+            case 'D':
+                Console.WriteLine("DEFICIENTE");
+                break;
+            case 'F':
+                Console.WriteLine("REPROBADO");
+                break;
+            default:
+                Console.WriteLine(" LETRA NO VALIDA!!");
+                break;
+        }
+
+
+
+    }
+}
